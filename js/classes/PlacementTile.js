691de03e3c74d4a7a058ffdc60700181b1ebda66
이유로 <eurohand@naver.com>
2023-04-25 20:36:45 +0900

    basic game

---
+++ js/classes/PlacementTile.js
@@ -0,0 +1,26 @@
+export default class PlacementTile {
+  constructor({ position = { x: 0, y: 0 } }) {
+    this.position = position;
+    this.size = 64;
+    this.color = "rgba(255, 255, 255, 0.15)";
+    this.isOccupied = false;
+  }
+
+  draw(c) {
+    c.fillStyle = this.color;
+    c.fillRect(this.position.x, this.position.y, this.size, this.size);
+  }
+
+  update(c, mouse) {
+    this.draw(c);
+
+    if (
+      mouse.x > this.position.x &&
+      mouse.x < this.position.x + this.size &&
+      mouse.y > this.position.y &&
+      mouse.y < this.position.y + this.size
+    ) {
+      this.color = "white";
+    } else this.color = "rgba(255, 255, 255, 0.15)";
+  }
+}
